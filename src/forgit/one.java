041903e4f0beb867234310b6ec56726c1ca3086f
Kiran Kompelli <kompellikiran15241@gmail.com>
2025-07-24 12:19:19 +0530

    new-branch

--- src/forgit/one.java
+++ src/forgit/one.java
@@ -5,7 +5,7 @@ public class one {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-     System.out.println("one.java");
+     System.out.println("two.java");
     
 
      System.out.println("one.java file");
